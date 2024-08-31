7f93db16c1101643a4f2a13efe3465b43ef1b86c
Orangev4 <aaravikrishna30@gmail.com>
2024-08-31 20:33:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7362034123:AAHIvgrnxoE_bgPhinknAJhbj7CMnUWZg0s")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Luis_enriquei")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5184635355")
 
 
 
